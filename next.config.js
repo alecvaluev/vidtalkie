13b14d5937a52937130d2eb9160c4adddaa80289
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-14 17:48:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['tvline.com', 'lh3.googleusercontent.com', 'cdn2.jazztimes.com']
-  }
+  },
+  typescript; {
+    ignoreBuildErrora: true,
 }
 
 module.exports = nextConfig
