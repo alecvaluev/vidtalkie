03769eb5bc1f2fd13723788116bad53dcd77330e
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-16 13:44:37 -0400

    Update Comments.tsx

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -29,8 +29,8 @@ const Comments = ({comment, setComment, addComment, comments, isPostingComment}:
   const { userProfile, allUsers } = useAuthStore();
 
   return (
-    <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
-      <div className='overflow-scroll lg:h-[475px]'>
+    <div className='border-t-2 border-gray-200 pt-4 pl-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
+      <div className='overflow-scroll lg:h-[485px]'>
         {
           comments?.length ? (
               comments.map((comment, idx) => (
