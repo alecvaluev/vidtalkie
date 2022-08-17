92f79bbd0e7311ea62ca532671cee82d98fa78f3
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-16 22:53:29 -0400

    Update CommetsButton.tsx

--- components/CommetsButton.tsx
+++ components/CommetsButton.tsx
@@ -2,23 +2,17 @@
 import React from 'react';
 //import icons
 import { AiFillMessage } from 'react-icons/ai';
+import SimpleButton from './SimpleButton';
 
 interface IProps {
-  comments: any[]
+  comments: any[],
+  col: boolean
 }
 
-function CommentsButton({comments}: IProps) {
-  
+function CommentsButton({comments, col = false}: IProps) {
   return (
-    <div className='flex gap-6 m-1'>
-      <div className='mt-4 me-4 flex items-center cursor-pointer'>
-        <div className='bg-primary rounded-full p-3 justify-center'>
-              <AiFillMessage className='text-lg md:text-2xl'/>
-        </div>
-        <p className='text-md font-semibold px-2'>{comments?.length | 0}</p>
-      </div>
-    </div>
+    <SimpleButton count={comments?.length} icon={<AiFillMessage />} col={col} clicked={false}/>
   )
 }
 
-export default CommentsButton
+export default CommentsButton;
