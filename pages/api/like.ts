64070b1e79073c854ff0037eca267785fe20cd08
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-16 23:33:33 -0400

    Update like.ts

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -25,7 +25,7 @@ export default async function handler(
         await client
             .patch(postId)
             .unset([`likes[_ref=="${userId}"]`])
-
+            .commit()
         res.status(200).json(data);
     }
 }
