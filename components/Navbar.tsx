a6d101362ccf22cc46933b8907ebe833be69bf5c
Alec <78243032+alecvaluev@users.noreply.github.com>
2022-08-16 20:11:32 -0400

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar(){
         }
     }
     return(
-        <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+        <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-1 px-4">
             <Link href='/'>
                 <div className="w-[100px] md:w-[130px]">
                     <span className={`text-2xl font-bold text-[#6B2D5C]`}>VidTalkie</span>
